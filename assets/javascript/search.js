01ea853b8e56ee0a1e1447d2d5da1fdd675bb363
PatricioHML <patriciohml@hotmail.com>
2018-05-19 13:49:55 -0500

    hola2

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,3 +1,18 @@
-var searchTerm = $("#searchTermText").val().trim();
 var apiKey = "12a836ed9e574203bfb9d14a3e19a280";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+ apiKey + "?q=" + searchTerm;
+var searchTerm;
+
+$("#search").on("click", function(event) {
+    searchTerm = $("#searchTermText").val().trim();
+    event.preventDefault();
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+          console.log(response);
+    
+      });
+    //console.log(searchTerm);
+
+});
+
