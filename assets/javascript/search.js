29386529ce08a617eef3fb83f90d715dd2135e06
PatricioHML <patriciohml@hotmail.com>
2018-05-19 13:14:07 -0500

    sup

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,3 +1,3 @@
-var searchTerm = $("#searchTermText");
+var searchTerm = $("#searchTermText").val().trim();
 var apiKey = "12a836ed9e574203bfb9d14a3e19a280";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+ apiKey + "?q=" + searchTerm;
